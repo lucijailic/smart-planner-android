372878369955efb98c0f4c8a7f5cef6afdd2448b
Lucija Ilić <lucijailic120@gmail.com>
2026-09-03 12:59:24 +0200

    Implement main navigation with bottom navigation

---
+++ app/src/main/java/com/smartplanner/app/fragments/TasksFragment.java
@@ -0,0 +1,12 @@
+package com.smartplanner.app.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.smartplanner.app.R;
+
+public class TasksFragment extends Fragment {
+
+    public TasksFragment() {
+        super(R.layout.fragment_tasks);
+    }
+}
